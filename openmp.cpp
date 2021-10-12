852bdb8d4378eda5e5719399dbd383a86cd8cd38
touhiDroid <tz08128@gmail.com>
2021-10-12 19:43:20 -0400

    makefile fixed, all but cuda files are separated

--- openmp.cpp
+++ openmp.cpp
@@ -12,19 +12,11 @@
 #include "libarff/arff_parser.h"
 #include "libarff/arff_data.h"
 #include <bits/stdc++.h>
+#include <omp.h>
 #include <pthread.h>
 
 using namespace std;
 
-struct ThreadArgs {
-    ArffData *train;
-    ArffData *test;
-    int num_classes;
-    int k;
-    int start;
-    int end;
-    int *predictions;
-};
 
 float distance(ArffInstance *a, ArffInstance *b) {
     float sum = 0;
@@ -63,22 +55,62 @@ float computeAccuracy(int *confusionMatrix, ArffData *dataset) {
     return successfulPredictions / (float) dataset->num_instances();
 }
 
-void *KnnRunSingleThread(void *arguments) {
-    struct ThreadArgs *argus = (struct ThreadArgs *) arguments;
-    int k = argus->k;
+/*float distance(int32 size, float *a, float *b) {
+    float sum = 0;
 
-    for (int queryIndex = argus->start; queryIndex < argus->end; queryIndex++) {
+    for (int i = 0; i < size; i++) {
+        float diff = (a[i] - b[i]);
+        sum += diff * diff;
+    }
+
+    return sum;
+}
+
+int *computeConfusionMatrix(int *predictions, int nClasses, int nInstances, int32 *lastAttributesArr) {
+    int *confusionMatrix = (int *) calloc(nClasses * nClasses, sizeof(int));
+
+    for (int i = 0; i < nInstances; i++) { // for each instance compare the true class and predicted class
+        int trueClass = lastAttributesArr[i];
+        int predictedClass = predictions[i];
+
+        confusionMatrix[trueClass * nClasses + predictedClass]++;
+    }
+    return confusionMatrix;
+}
+
+float computeAccuracy(int *confusionMatrix, int nClasses, int nInstances) {
+    int successfulPredictions = 0;
+
+    for (int i = 0; i < nClasses; i++) {
+        successfulPredictions += confusionMatrix[i * nClasses + i]; // elements in the diagonal are correct predictions
+    }
+
+    return successfulPredictions / ((float) nInstances * 1.0);
+}*/
+
+
+int *KNNOpenMP(ArffData *train, ArffData *test, int k, int num_desired_threads) {
+    // omp_set_num_threads(128);
+    // Implements a sequential kNN where for each candidate query an in-place priority queue is maintained to identify the kNN's.
+
+    // predictions is the array where you have to return the class predicted (integer) for the test dataset instances
+    int *predictions = (int *) malloc(test->num_instances() * sizeof(int));
+
+    int num_classes = train->num_classes();
+
+#pragma omp parallel for num_threads(num_desired_threads)
+    for (int queryIndex = 0; queryIndex < test->num_instances(); queryIndex++) {
 
         // stores k-NN candidates for a query vector as a sorted 2d array. First element is inner product, second is class.
         float *candidates = (float *) calloc(k * 2, sizeof(float));
         for (int i = 0; i < 2 * k; i++) { candidates[i] = FLT_MAX; }
 
         // Stores bincounts of each class over the final set of candidate NN
-        int *classCounts = (int *) calloc(argus->num_classes, sizeof(int));
+        int *classCounts = (int *) calloc(num_classes, sizeof(int));
 
-        for (int keyIndex = 0; keyIndex < argus->train->num_instances(); keyIndex++) {
+        for (int keyIndex = 0; keyIndex < train->num_instances(); keyIndex++) {
 
-            float dist = distance(argus->test->get_instance(queryIndex), argus->train->get_instance(keyIndex));
+            float dist = distance(test->get_instance(queryIndex), train->get_instance(keyIndex));
 
             // Add to our candidates
             for (int c = 0; c < k; c++) {
@@ -92,8 +124,8 @@ void *KnnRunSingleThread(void *arguments) {
 
                     // Set key vector as potential k NN
                     candidates[2 * c] = dist;
-                    candidates[2 * c + 1] = argus->train->get_instance(keyIndex)->get(
-                            argus->train->num_attributes() - 1)->operator float(); // class value
+                    candidates[2 * c + 1] = train->get_instance(keyIndex)->get(
+                            train->num_attributes() - 1)->operator float(); // class value
 
                     break;
                 }
@@ -107,54 +139,17 @@ void *KnnRunSingleThread(void *arguments) {
 
         int max = -1;
         int max_index = 0;
-        for (int i = 0; i < argus->num_classes; i++) {
+        for (int i = 0; i < num_classes; i++) {
             if (classCounts[i] > max) {
                 max = classCounts[i];
                 max_index = i;
             }
         }
 
-        argus->predictions[queryIndex] = max_index;
+        predictions[queryIndex] = max_index;
 
         for (int i = 0; i < 2 * k; i++) { candidates[i] = FLT_MAX; }
-        memset(classCounts, 0, argus->num_classes * sizeof(int));
-    }
-
-    pthread_exit(0);
-}
-
-int *KNNThreaded(ArffData *train, ArffData *test, int k, int num_desired_threads) {
-    // Implements a sequential kNN where for each candidate query an in-place priority queue is maintained to identify the kNN's.
-
-    // predictions is the array where you have to return the class predicted (integer) for the test dataset instances
-    int *predictions = (int *) malloc(test->num_instances() * sizeof(int));
-
-    int num_classes = train->num_classes();
-
-    // Create multiple threads for the outer loop
-    int n = test->num_instances();
-    // int partition_size = (n + num_desired_threads - 1) / num_desired_threads;
-    pthread_t *threads = (pthread_t *) malloc(num_desired_threads * sizeof(pthread_t));
-
-    // running 1 thread short to let the last one collect all the remaining data points
-    for (int t = 0; t < num_desired_threads; t++) {
-        int start = t * n / num_desired_threads;
-        int end = (t + 1) * n / num_desired_threads;
-        if (t == num_desired_threads - 1)
-            end = n;
-        // call KnnRunOneThread
-        struct ThreadArgs argus;
-        argus.train = train;
-        argus.test = test;
-        argus.num_classes = num_classes;
-        argus.k = k;
-        argus.start = start;
-        argus.end = end;
-        argus.predictions = predictions;
-        pthread_create(&threads[t], NULL, KnnRunSingleThread, (void *) &argus);
-    }
-    for (int t = 0; t < num_desired_threads; t++) {
-        pthread_join(threads[t], NULL);
+        memset(classCounts, 0, num_classes * sizeof(int));
     }
 
     return predictions;
@@ -162,7 +157,7 @@ int *KNNThreaded(ArffData *train, ArffData *test, int k, int num_desired_threads
 
 int main(int argc, char *argv[]) {
     if (argc != 5) {
-        cout << "Usage: ./multithreaded.o datasets/train.arff datasets/test.arff k NUM_DESIRED_THREADS"
+        cout << "Usage: ./openmp.o datasets/train.arff datasets/test.arff k NUM_DESIRED_THREADS"
                 "\n\t\tk = 3 (typically)\n\t\tNUM_DESIRED_THREADS = 1,2,4,8,16,32,64 or 128" << endl;
         exit(0);
     }
@@ -180,16 +175,17 @@ int main(int argc, char *argv[]) {
     int *predictions = NULL;
 
     clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    predictions = KNNThreaded(train, test, k, num_desired_threads);
+    predictions = KNNOpenMP(train, test, k, num_desired_threads);
     clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-
     int *confusionMatrix = computeConfusionMatrix(predictions, test);
     float accuracy = computeAccuracy(confusionMatrix, test);
-
     uint64_t diff = (1000000000L * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec) / 1e6;
 
     printf("%i-NN  -  %lu test  - %lu train  -  %llu ms  -   %.4f%%\n", k,
            test->num_instances(), train->num_instances(), (long long unsigned int) diff, accuracy);
+    /*printf("The %i-NN classifier for %lu test instances on %lu train instances required %llu ms CPU time. "
+           "Accuracy was %.4f\n", k, test->num_instances(), train->num_instances(),
+           (long long unsigned int) diff, accuracy);*/
 
     return 0;
 }
